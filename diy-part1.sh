dcc53de7982c856134860cb846254d90d3e30510
jackyssion <jackyssion@users.noreply.github.com>
2021-06-08 07:05:35 +0800

    添加Server酱推送源

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add serverchan Server酱推送
+git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
